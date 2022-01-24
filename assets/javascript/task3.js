ba8c182c32374d7be8e9741bc246ddd2579ac1ab
aalyiiahh <aalyiah.gallagher@vtsd.com>
2022-01-24 09:41:38 -0500

    test completed

--- assets/javascript/task3.js
+++ assets/javascript/task3.js
@@ -1,1 +1,8 @@
 /* Put code for the task below */
+function changetext(){
+    var text = document.getElementById("BDText");
+    text.style.display = "none";
+
+    var newText = document.getElementById("BDNewText");
+    newText.style.display =  "block";
+}
